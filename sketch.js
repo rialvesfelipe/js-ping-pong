de97494fe73cf0155f739fe030e8f941a5a1eaec
rialvesfelipe <00001094583376sp@al.educacao.sp.gov.br>
2024-05-28 17:29:45 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,20 @@
+//variáveis da bolinha
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = 22;
+let raio = diametro / 2;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//variáveis do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+let velocidadeYOponente;
